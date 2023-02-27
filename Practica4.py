76992a2f1f2790237117ca0fbfe26aa1d188231b
FranciscoDRG <pancho221299@gmail.com>
2023-02-26 22:46:47 -0600

    push final

--- Practica4.py
+++ Practica4.py
@@ -4,8 +4,8 @@ total = 0
 while x == "si":
     d [input("Ingrese la materia: ")] = input("Ingrese el numero de creditos: ")
     x = input("Seguir capturando?")
-
 for m in d:
     print("La materia",m,"tiene",d.get(m),"creditos")
     total += int(d.get(m))
 print("Con una suma total de creditos de:",total)
+
--- Practica6.py
+++ Practica6.py
@@ -3,3 +3,4 @@ numeros = {0:"Cero",1:"Uno",2:"Dos",3:"Tres",4:"Cuatro",5:"Cinco",
            12:"Doce",13:"Trece",14:"Catorce",15:"Quince",16:"Dieciseis",
            17:"Diecisiete",18:"Dieciocho",19:"Diecinueve",20:"Veinte"}
 print(numeros.get(int(input("Ingrese un número entre 0 y 20: "))))
+
