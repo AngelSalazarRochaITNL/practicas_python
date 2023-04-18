744a9dba884238879691c7e4d081667840865404
FranciscoDRG <pancho221299@gmail.com>
2023-04-18 07:37:47 -0500

    .

--- Practica1.py
+++ Practica1.py
@@ -1,7 +1,13 @@
 
-def multiplicar(*args):
-    resultado = 1
-    for valor in args:
-        resultado *=valor
-    return resultado
-print("Multiplicación de los numeros; ",multiplicar(1,2,3,4,5,6,7))
+# def multiplicar(*args):
+#     resultado = 1
+#     for valor in args:
+#         resultado *=valor
+#     return resultado
+# print("Multiplicación de los numeros; ",multiplicar(1,2,3,4,5,6,7))
+
+a:float = 0.3
+b:float = 0.2
+c:float = a -b
+print(c) 
+
